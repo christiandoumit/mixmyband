b459a99ece3eb8161aa7b338b79b02c37ed985d4
Quan Nguyen <quanguyen250@gmail.com>
2019-11-27 14:21:27 -0500

    Started the home page with sign in/sign out/sign up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
   def home
-    skip_before_action :authenticate_user!, only: :home
   end
+
 end
