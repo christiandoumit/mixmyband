815bc44d0be4597570c74490f826dbe6deb18d2a
Quan Nguyen <quanguyen250@gmail.com>
2019-11-27 17:44:26 -0500

    Modified view file for the artist and modified the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,37 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # brain_flower = User.create!(first_name: "Theo", last_name: "Poncet", type_of_user: "Artist", artist_name: "Brain Flower", music_type: "Alternative Rock", artist_type: "Professional Band", price: "350$", fb_url: "https://www.facebook.com/BrainflowerBand", youtube_url: "https://www.youtube.com/channel/UC8EpshxnqopvKFqoTFaPPzA")
-
+puts "Deleting previous database"
 User.destroy_all
+puts "Previous database cleared"
 
 puts "Creating faker database"
 40.times do
   User.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
-    type_of_user: Faker::Quote.yoda,
+    type_of_user: ["artist", "eventer"].sample,
     artist_name: Faker::Music.band,
     music_type: Faker::Music.genre,
     price: Faker::Number.positive,
     fb_url: Faker::Internet.url,
     youtube_url: Faker::Internet.url,
-    crowd_type: Faker::Superhero.name,
-    email: Faker::Internet.email,
-    password: "123456"
-    )
-end
-
-40.times do
-  User.create!(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    type_of_user: Faker::Restaurant.type,
-    artist_name: Faker::Restaurant.name,
-    music_type: Faker::Music.genre,
-    price: Faker::Number.positive,
-    fb_url: Faker::Internet.url,
-    youtube_url: Faker::Internet.url,
-    venue_address: Faker::Address.full_address,
+    crowd_type: ["young", "teen", "elders", "professionals", "hipsters", "connoisseurs", "family", "other"].sample,
     email: Faker::Internet.email,
     password: "123456"
     )
