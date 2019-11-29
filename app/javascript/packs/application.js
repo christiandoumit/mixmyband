a32502a412bcd7ffa76bc86204e4f4ff126a7cd1
Vi <vipannetier@gmail.com>
2019-11-29 12:13:55 -0500

    filters: done dropdowns and some JS on it

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,8 @@
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
+import { filterEvents } from '../plugins/search';
+
 initMapbox();
+filterEvents();
 // import '../plugins/init_mapbox';
