8e04eb2ed8447246670c024f5e8db70f5dc553e9
Vi <vipannetier@gmail.com>
2019-11-28 21:56:52 -0500

    map started, need to make the popup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  geocoded_by :venue_address
+  after_validation :geocode, if: :will_save_change_to_venue_address?
   has_many :audio_urls
   has_many :bookings
   has_many :images
