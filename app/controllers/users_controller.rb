240fa0c4bd1d9e1d74b3c61f5592023a12995f98
Quan Nguyen <quanguyen250@gmail.com>
2019-12-02 11:49:02 -0500

    displayed all the artist in every category

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,19 @@
 class UsersController < ApplicationController
   def index
-
-    # raise
-    # @all_users = User.all
     @users = User.geocoded
+
     if params[:Type_of_crowd].present?
-      @users = @users.where(crowd_type: params[:Type_of_crowd] )
+      @users = @users.where(crowd_type: params[:Type_of_crowd])
     end
+
     if params[:Type_of_venue].present?
-      @users = @users.where(venue_type: params[:Type_of_venue] )
+      @users = @users.where(venue_type: params[:Type_of_venue])
     end
+
     @eventers = @users.filter { |user| user.type_of_user == 'eventer' }
-    @artists = @users.filter { |user| user.type_of_user == 'artist' }
+    @artists = User.where(type_of_user: 'artist')
 
-
-    @markers = @eventers.map  do |user|
+    @markers = @eventers.map do |user|
       {
         lat: user.latitude,
         lng: user.longitude,
@@ -24,7 +23,6 @@ class UsersController < ApplicationController
   end
 
   def show
-
     @user = User.find(params[:id])
 
     @markers = [
@@ -33,6 +31,5 @@ class UsersController < ApplicationController
         lng: @user.longitude,
         infoWindow: render_to_string(partial: "info_window", locals: { user: @user })
       }]
-
   end
 end
