5d067d2252e24e2f8c587ceca81a760519824453
Vi <vipannetier@gmail.com>
2019-12-04 17:01:45 -0500

    sweetalert for dashboard

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,18 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 import { filterEvents, filterArtists } from '../plugins/search';
+import { initSweetalert } from '../plugins/init_sweetalert';
 
 initMapbox();
 filterEvents();
 filterArtists();
 // import '../plugins/init_mapbox';
+
+
+initSweetalert('.booking-icons-item.fas.fa-check-circle.fa-lg', {
+  title: "It's a mix!",
+  text: "Your confirmation has been sent.",
+  icon: "success"
+}, (value) => {
+  window.location.reload(true);
+});
