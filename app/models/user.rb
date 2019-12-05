bf11c6d73747c6c4157d7feec5845bdbeb3467fe
Christian Doumit <christian.doumit@gmail.com>
2019-12-04 23:26:33 -0500

    Minor modifications

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,6 @@ class User < ApplicationRecord
   has_many :sent_bookings, class_name: "Booking", foreign_key: :sender_id, dependent: :destroy
   has_many :received_bookings, class_name: "Booking", foreign_key: :receiver_id, dependent: :destroy
 
-
   validates :music_type, inclusion: { in: CATEGORIES + [nil] }
   validates :artist_type, inclusion: { in: ARTISTTYPE + [nil] }
   validates :event_type, inclusion: { in: TYPEOFEVENT + [nil] }
