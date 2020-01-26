8d6ae52fe9f2ad82b66bbb3b6e55efc4d482cb2d
Quan Nguyen <quanguyen250@gmail.com>
2020-01-26 01:48:30 -0500

    README file update

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # MixMyBand
 MixMyBand is an app for every music lover, helping an Artist to mix his music with an Eventer's venue that fits his style & vice-versa!
+
+# Built with
+Ruby on Rails - The web framework used
+Ruby - Programming Language
+JavaScript - Programming Language
+BootStap - CSS framework used
+
+# Authors
+Christian Doumit
+Quan Nguyen
+Vi Pannetier
+Costin Dumitrescu
+
--- app/javascript/plugins/search.js
+++ app/javascript/plugins/search.js
@@ -1,30 +1,26 @@
 const filterEvents = () => {
   const venue = document.querySelector("#Type_of_venue");
   const crowd = document.querySelector("#Type_of_crowd");
-
   if (venue) {
     venue.addEventListener("input", () => {
       submitForm();
     })
-
     crowd.addEventListener("input", () => {
       submitForm();
-    })
+    });
   }
 }
 
 const filterArtists = () => {
   const artist_type = document.querySelector("#Artist_type");
   const price = document.querySelector("#Price");
-
   if (artist_type) {
     artist_type.addEventListener("input", () => {
       submitForm();
-    })
-
+    });
     price.addEventListener("input", () => {
       submitForm();
-    })
+    });
   }
 }
 
